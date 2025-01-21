a38208faf814bc657a34a87fd4c3b9514909e889
sandeep khariwal <sandeepkharival123@gmail.com>
2025-01-21 20:52:47 +0530

    error resolved

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -77,7 +77,7 @@ export const TestimonialsData:Testimonial[] = [
     
   ];
 
-export default function page() {
+export default function Page() {
   return (
     <div>
       <Testimonials testimonials={TestimonialsData}/>
